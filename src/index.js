c2869f6027be52f4d8ec30781eb51bed00498955
yungshun317 <seraph317@gmail.com>
2023-03-28 19:44:38 +0800

    [FEAT] Style the block and add hover effects

--- src/index.js
+++ src/index.js
@@ -26,21 +26,22 @@ function EditComponent(props) {
 
     return (
         <div className="quiz-edit-block">
-            <TextControl label="Question:" />
-            <p>Answers:</p>
+            <TextControl label="Question:" style={{ fontSize: "20px" }} />
+            <p style={{ fontSize: "13px", margin: "20px 0 8px 0" }}>Answers:</p>
             <Flex>
                 <FlexBlock>
                     <TextControl />
                 </FlexBlock>
                 <FlexItem>
                     <Button>
-                        <Icon icon="star-empty" />
+                        <Icon className="mark-as-correct" icon="star-empty" />
                     </Button>
                 </FlexItem>
                 <FlexItem>
-                    <Button>Delete</Button>
+                    <Button isLink className="answer-delete">Delete</Button>
                 </FlexItem>
             </Flex>
+            <Button isPrimary>Add another answer</Button>
         </div>
     )
     /*
