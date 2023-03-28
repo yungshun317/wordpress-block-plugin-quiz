86dd5f3197990eb26abdb78a35b19db51943e840
yungshun317 <seraph317@gmail.com>
2023-03-28 19:37:38 +0800

    [FEAT] Import and use the WordPress components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import "./index.scss"
+import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon} from "@wordpress/components"
+
 wp.blocks.registerBlockType("plugin/quiz", {
     title: "Quiz",
     icon: "smiley",
@@ -6,28 +9,48 @@ wp.blocks.registerBlockType("plugin/quiz", {
         skyColor: { type: "string" },
         grassColor: { type: "string" }
     },
-    edit: function(props) {
-        function updateSkyColor(event) {
-            props.setAttributes({ skyColor: event.target.value })
-        }
-
-        function updateGrassColor(event) {
-            props.setAttributes({ grassColor: event.target.value })
-        }
-
-        return (
-            <div>
-                <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
-                <input type="text" placeholder="Grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
-            </div>
-        )
-    },
+    edit: EditComponent,
     save: function(props) {
         return null
-        /*
-        return (
-            <p>Today the sky is <span className="skyColor">{props.attributes.skyColor}</span> and the grass is <span className="grassColor">{props.attributes.grassColor}</span>.</p>
-        )
-         */
     }
 })
+
+function EditComponent(props) {
+    function updateSkyColor(event) {
+        props.setAttributes({ skyColor: event.target.value })
+    }
+
+    function updateGrassColor(event) {
+        props.setAttributes({ grassColor: event.target.value })
+    }
+
+    return (
+        <div className="quiz-edit-block">
+            <TextControl label="Question:" />
+            <p>Answers:</p>
+            <Flex>
+                <FlexBlock>
+                    <TextControl />
+                </FlexBlock>
+                <FlexItem>
+                    <Button>
+                        <Icon icon="star-empty" />
+                    </Button>
+                </FlexItem>
+                <FlexItem>
+                    <Button>Delete</Button>
+                </FlexItem>
+            </Flex>
+        </div>
+    )
+    /*
+    return (
+        <div>
+
+            <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
+            <input type="text" placeholder="Grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
+
+        </div>
+    )
+     */
+}
